be6952459dbf7ef3a9c539ab476e81c3d836a638
Couto <jtcouto13@gmail.com>
2026-08-17 21:13:21 -0300

    Refactor vertex transformation and rendering logic
    Refactor translrot-sketch.js for improved clarity and functionality, including changes to vertex transformations and rendering logic.

--- aula3/translrot-sketch.js
+++ aula3/translrot-sketch.js
@@ -1,82 +1,129 @@
-let L, o, p, I, t, w;
+let L, o, I, t;
+let facesBase;
 
 function setup() {
   L = 220;
   t = 0;
-  o = [0, 0, 0];
+  o = [20, 20, 20];
   createCanvas(L, L, WEBGL);
   orbitControl();
-  p = [20, 20, 20];
-  I = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+
+  // Vértices distribuídos nos eixos X, Y e Z (Tetraedro Tridimensional)
+  let v0 = [30, 0, 0];
+  let v1 = [0, 30, 0];
+  let v2 = [0, 0, 30];
+  let v3 = [30, 30, 30];
+
+  // Decomposição em faces triangulares (garante coplanaridade e elimina piscamentos)
+  facesBase = [
+    [v0, v1, v2],
+    [v0, v1, v3],
+    [v1, v2, v3],
+    [v2, v0, v3]
+  ];
+
+  I = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ];
 }
 
 function draw() {
-  function plano(L) {
-    background(220); // Ajustado para não usar a variável global de escopo L incorretamente como cor
-    line(L/2, 0, 0, -L/2, 0, 0);
-    line(0, L/2, 0, 0, -L/2, 0);
-    line(0, 0, L/2, 0, 0, -L/2);
-  }
-  plano(L);
+  background(220);
 
-  function vxe(v, e) {
-    for(i=0;i<3;i++) {
-      v[i] = v[i]*e;
-    }
-    return v;
-  }
-
-  function vxa(v, a) {
-    let b = [];
-    for (let i = 0; i < 3; i++) {
-      b[i] = [];
-      for (let j = 0; j < 3; j++) {
-        b[i][j] = v[i] * a[i][j];
-      }
-    }
-    return b;
-  }
-
-  function axa(a, b) {
-    let c = [[0,0,0], [0,0,0], [0,0,0]];
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        for (let k = 0; k < 3; k++) {
-          c[i][j] += a[i][k] * b[k][j];
-        }
-      }
-    }
-    return c;
-  }
-
-  function rot(a, xang, yang, zang) {
-    let xrot = [[1, 0, 0],
-                [0, cos(xang), -sin(xang)],
-                [0, sin(xang), cos(xang)]];
-    
-    let yrot = [[cos(yang), 0, sin(yang)],
-                [0, 1, 0],
-                [-sin(yang), 0, cos(yang)]];
-    
-    let zrot = [[cos(zang), -sin(zang), 0],
-                [sin(zang), cos(zang), 0],
-                [0, 0, 1]];
-
-    let matrizCombinada = axa(xrot, yrot);
-    matrizCombinada = axa(matrizCombinada, zrot);
-
-    return axa(a, matrizCombinada);
-  }
-
-  function piramide(a) {
-    for (let i = 0; i < 3; i++) {
-      line(...o,...a[i]);
-      if (i<2)    line(...a[i],...a[i+1]);
-    }
-    line(...a[2],...a[0]);
-  }
+  // Ajuste de ângulo de observação para perspectiva nos três eixos (X, Y, Z)
+  rotateX(-PI / 6);
+  rotateY(PI / 4);
 
   t++;
-  m = rot(vxa(p,I),...vxe([1/200,1/300,1/400],t*2*PI));
-  piramide(m);
+  let wrot = vxe([0, 0, 1 / 400], 2 * PI);
+  let wtrans = vxe([0, 0, 1 / 400], 2 * PI);
+
+  stroke(0);
+  strokeWeight(1.5);
+  fill(150, 200, 255, 230); // Preenchimento com opacidade para suavizar intersecções
+
+  // Renderização simplicial por primitivas triangulares
+  for (let f = 0; f < facesBase.length; f++) {
+    beginShape(TRIANGLES);
+    for (let i = 0; i < facesBase[f].length; i++) {
+      let verticeTransformado = transformarVertice(facesBase[f][i], o, wrot, wtrans, t);
+      vertex(...verticeTransformado);
+    }
+    endShape(CLOSE);
+  }
+}
+
+function vxe(v, e) {
+  let res = [];
+  for (let i = 0; i < v.length; i++) {
+    res[i] = v[i] * e;
+  }
+  return res;
+}
+
+function vxa(v, a) {
+  let res = [0, 0, 0];
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      res[j] += v[i] * a[i][j];
+    }
+  }
+  return res;
+}
+
+function axa(a, b) {
+  let c = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      for (let k = 0; k < 3; k++) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+  return c;
+}
+
+function rotVetor(v, xang, yang, zang) {
+  let xrot = [
+    [1, 0, 0],
+    [0, cos(xang), -sin(xang)],
+    [0, sin(xang), cos(xang)]
+  ];
+  let yrot = [
+    [cos(yang), 0, sin(yang)],
+    [0, 1, 0],
+    [-sin(yang), 0, cos(yang)]
+  ];
+  let zrot = [
+    [cos(zang), -sin(zang), 0],
+    [sin(zang), cos(zang), 0],
+    [0, 0, 1]
+  ];
+
+  let R = axa(axa(xrot, yrot), zrot);
+  return vxa(v, R);
+}
+
+function deslVetor(v, deslocamento) {
+  let res = [];
+  for (let i = 0; i < 3; i++) {
+    res[i] = v[i] + deslocamento[i];
+  }
+  return res;
+}
+
+function transformarVertice(v, orig, wrot, wtrans, t) {
+  let angRot = vxe(wrot, t);
+  let angTrans = vxe(wtrans, t);
+
+  let vRot = rotVetor(v, ...angRot);
+  let deslocamento = rotVetor(orig, ...angTrans);
+
+  return deslVetor(vRot, deslocamento);
 }
